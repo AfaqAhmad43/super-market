37527e54bc3c1d98c599541481b5f2a6bbcd860c
AfaqAhmad43 <superpsycho4347@gmail.com>
2025-12-10 16:15:21 +0500

    Add project files.

---
+++ GoMartApplication/DBConnect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GoMartApplication
+{
+    class DBConnect
+    {
+        private SqlConnection con = new SqlConnection(@"data source=DESKTOP-8B4GOKA\SQLEXPRESS;initial catalog=GoMartDB;trusted_connection=true");
+        public SqlConnection GetCon()
+        {
+            return con;
+        }
+        public void OpenCon()
+        {
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void CloseCon()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
